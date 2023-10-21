3b9ff7600a12082c59edd77c8dde828f1f5797fa
Naman1997 <aroranaman17@gmail.com>
2023-10-21 22:31:27 +0530

    Bug fix

--- main.tf
+++ main.tf
@@ -18,78 +18,78 @@ provider "proxmox" {
   pm_tls_insecure = true
 }
 
-resource "aws_key_pair" "wg_key" {
-  key_name   = "wg-keypair"
-  public_key = file(var.ssh_public_key)
-  tags = {
-    Name = "WireGuard K8s Ingress"
-  }
-}
+# resource "aws_key_pair" "wg_key" {
+#   key_name   = "wg-keypair"
+#   public_key = file(var.ssh_public_key)
+#   tags = {
+#     Name = "WireGuard K8s Ingress"
+#   }
+# }
 
-resource "aws_security_group" "wg_sg" {
-  name        = "wg-security-group"
-  description = "wg security group for SSH, HTTP, HTTPS and WireGuard"
+# resource "aws_security_group" "wg_sg" {
+#   name        = "wg-security-group"
+#   description = "wg security group for SSH, HTTP, HTTPS and WireGuard"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
-  }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "Allow all outbound traffic"
+#   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "SSH access"
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "SSH access"
+#   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "HTTP access"
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "HTTP access"
+#   }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "HTTPS access"
-  }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "HTTPS access"
+#   }
 
-  ingress {
-    from_port   = var.wireguard_port
-    to_port     = var.wireguard_port
-    protocol    = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "WireGuard access"
-  }
+#   ingress {
+#     from_port   = var.wireguard_port
+#     to_port     = var.wireguard_port
+#     protocol    = "udp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = "WireGuard access"
+#   }
 
-  tags = {
-    Name = "WireGuard K8s Ingress"
-  }
-}
+#   tags = {
+#     Name = "WireGuard K8s Ingress"
+#   }
+# }
 
-resource "aws_instance" "wg_instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = aws_key_pair.wg_key.key_name
-  security_groups = [aws_security_group.wg_sg.name]
-  user_data = var.user_data
+# resource "aws_instance" "wg_instance" {
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+#   key_name      = aws_key_pair.wg_key.key_name
+#   security_groups = [aws_security_group.wg_sg.name]
+#   user_data = var.user_data
 
-  root_block_device {
-    volume_size = 8
-  }
+#   root_block_device {
+#     volume_size = 8
+#   }
 
-  tags = {
-    Name = "WireGuard K8s Ingress"
-  }
-}
+#   tags = {
+#     Name = "WireGuard K8s Ingress"
+#   }
+# }
 
 resource "null_resource" "versions" {
   provisioner "remote-exec" {
@@ -101,33 +101,33 @@ resource "null_resource" "versions" {
     }
 
     inline = [
-      "ssh",
-      "${var.PROXMOX_USERNAME}@${var.PROXMOX_IP}",
-      "-i",
-      "${var.SSH_PRIVATE_KEY}",
+      "rm -f /tmp/alpine_version",
       "latest_version=\"$(pveam available | grep alpine | awk '{print $2}' | sort -V | tail -n 1)\"",
-      "echo \"{\\\"version\\\":\\\"$latest_version\\\"}\" > /tmp/data"
+      "echo \"{\\\"version\\\":\\\"$latest_version\\\"}\" > /tmp/alpine_version"
     ]
   }
 }
 
 data "external" "version" {
+  depends_on = [null_resource.versions]
   program = [
     "ssh",
     "${var.PROXMOX_USERNAME}@${var.PROXMOX_IP}",
     "-i",
     "${var.SSH_PRIVATE_KEY}",
-    "cat /tmp/data"
+    "cat /tmp/alpine_version"
   ]
 }
-
 
 locals {
   alpine_version = data.external.version.result["version"]
 }
 
-
 resource "null_resource" "create_template" {
+  depends_on = [
+    null_resource.versions,
+    data.external.version
+  ]
   provisioner "remote-exec" {
     when = create
     connection {
@@ -136,8 +136,8 @@ resource "null_resource" "create_template" {
       password = var.PROXMOX_PASSWORD
     }
 
-    inline = [ 
-      "pveam download ${var.TEMPLATE_STORAGE} ${local.alpine_version}" 
+    inline = [
+      "pveam download ${var.TEMPLATE_STORAGE} ${local.alpine_version}"
     ]
   }
 }
