12ed21891407720272a83c50451983fa6ec9ef37
Naman1997 <aroranaman17@gmail.com>
2024-09-07 20:07:59 +1000

    Uncomment steps

--- scripts/wireguard.sh
+++ scripts/wireguard.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Setup DuckDNS
-# ansible-playbook -v ansible/1-duckdns.yml -i ansible_hosts -e "@ansible/ansible_vars"
-# DUCKDNS_SETUP=$?
-# if [ $DUCKDNS_SETUP -ne 0 ]; then
-#     echo "Unable to create docker container for duckdns. Make sure docker is installed on the VPS."
-#     exit 1
-# fi
+ansible-playbook -v ansible/1-duckdns.yml -i ansible_hosts -e "@ansible/ansible_vars"
+DUCKDNS_SETUP=$?
+if [ $DUCKDNS_SETUP -ne 0 ]; then
+    echo "Unable to create docker container for duckdns. Make sure docker is installed on the VPS."
+    exit 1
+fi
 
-# # Setup unattended upgrades
-# ansible-playbook -v ansible/2-unattended-upgrades.yml -i ansible_hosts
-# UPGRADES_SETUP=$?
-# if [ $UPGRADES_SETUP -ne 0 ]; then
-#     echo "Unable to setup unattended upgrades. Make sure to use ubuntu/debian on both VMs."
-#     exit 1
-# fi
+# Setup unattended upgrades
+ansible-playbook -v ansible/2-unattended-upgrades.yml -i ansible_hosts
+UPGRADES_SETUP=$?
+if [ $UPGRADES_SETUP -ne 0 ]; then
+    echo "Unable to setup unattended upgrades. Make sure to use ubuntu/debian on both VMs."
+    exit 1
+fi
 
 # Setup wireguard
 ansible-playbook -v ansible/3-wireguard.yml -i ansible_hosts -e "@ansible/ansible_vars"
