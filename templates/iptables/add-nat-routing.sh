3a7e4da42d20a9520e4ef762ff0d404fcee02a39
Naman1997 <aroranaman17@gmail.com>
2024-09-10 00:25:15 +1000

    Keep 443 on cloud vps

--- templates/iptables/add-nat-routing.sh
+++ templates/iptables/add-nat-routing.sh
@@ -9,6 +9,6 @@ iptables -A FORWARD -i $WG_FACE -j ACCEPT
 iptables -t nat -A POSTROUTING -o $IN_FACE -j MASQUERADE
 iptables -I INPUT 1 -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
 
-# HTTP
+# HTTP(S)
 iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
 iptables -I INPUT 6 -m state --state NEW -p tcp --dport 443 -j ACCEPT
--- templates/iptables/remove-nat-routing.sh
+++ templates/iptables/remove-nat-routing.sh
@@ -9,6 +9,6 @@ iptables -D FORWARD -i $WG_FACE -j ACCEPT
 iptables -t nat -D POSTROUTING -o $IN_FACE -j MASQUERADE
 iptables -D INPUT -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
 
-# HTTP
+# HTTP(S)
 iptables -D INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
 iptables -D INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT
