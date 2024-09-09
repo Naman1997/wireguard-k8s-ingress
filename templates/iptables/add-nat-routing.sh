541c7182f2528dc7bf54245d72c72c39afe44118
Naman1997 <aroranaman17@gmail.com>
2024-09-10 00:14:19 +1000

    Remove use of port 443 from LAN

--- templates/iptables/add-nat-routing.sh
+++ templates/iptables/add-nat-routing.sh
@@ -9,6 +9,5 @@ iptables -A FORWARD -i $WG_FACE -j ACCEPT
 iptables -t nat -A POSTROUTING -o $IN_FACE -j MASQUERADE
 iptables -I INPUT 1 -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
 
-# HTTP(S)
+# HTTP
 iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
-iptables -I INPUT 6 -m state --state NEW -p tcp --dport 443 -j ACCEPT
--- templates/iptables/remove-nat-routing.sh
+++ templates/iptables/remove-nat-routing.sh
@@ -9,6 +9,5 @@ iptables -D FORWARD -i $WG_FACE -j ACCEPT
 iptables -t nat -D POSTROUTING -o $IN_FACE -j MASQUERADE
 iptables -D INPUT -i $IN_FACE -p udp --dport $WG_PORT -j ACCEPT
 
-# HTTP(S)
+# HTTP
 iptables -D INPUT -p tcp -m state --state NEW -m tcp --dport 80 -j ACCEPT
-iptables -D INPUT -p tcp -m state --state NEW -m tcp --dport 443 -j ACCEPT
